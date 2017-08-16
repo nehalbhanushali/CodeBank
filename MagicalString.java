65e265b30eb7c3cda122d9b5855e99fc5fabcac9
nehalbhanushali <bhanushali.n@husky.neu.edu>
2017-08-16 02:09:32 -0400

    Update MagicalString.java

--- MagicalString.java
+++ MagicalString.java
@@ -1,4 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codingQuestions;
 
+/**
+ *
+ * @author nehal
+ */
 
 public class Solution {
     public int magicalString(int n) {
