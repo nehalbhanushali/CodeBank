95b01a68003a0072d9b644cdef96098c988666d9
Nehal Bhanushali <bhanushali.n@husky.neu.edu>
2017-12-01 18:40:13 -0500

    Create SmallPalindrome.java

---
+++ SmallPalindrome.java
@@ -0,0 +1,13 @@
+/*
+
+var name = "nehalahen";
+
+
+function isPali(s){
+//console.log(s.charAt(6));
+for(var i=0;i<s.length;i++){
+if(s.charAt(i)!=s.charAt(s.length-i-1))
+return false;
+}
+return true;}
+*/
